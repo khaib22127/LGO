3a04ea995a6d2f2d16e5ebbcca01c5e4d9bb4730
khai bui <khaib22127@gmail.com>
2023-04-19 09:40:53 -0600

    fixing for render

--- backend/db/seeders/20230419001647-spotImages.js
+++ backend/db/seeders/20230419001647-spotImages.js
@@ -31,26 +31,6 @@ module.exports = {
           spotId: 5,
           url: "https://i.insider.com/5a25b75b3339b038008b45e0?width=750&format=jpeg&auto=webp",
         },
-        // {
-        //   spotId: 1,
-        //   url: "https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG9tZSUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-        //   preview: true,
-        // },
-        // {
-        //   spotId: 1,
-        //   url: "https://i0.wp.com/chronos-stores.com/wp-content/uploads/2022/05/how-to-use-statement-pieces-within-your-space-4-scaled.jpg?fit=2560%2C1709&ssl=1",
-        //   preview: true,
-        // },
-        // {
-        //   spotId: 1,
-        //   url: "https://media.istockphoto.com/id/1249281377/photo/galley-between-family-room-and-kitchen.jpg?s=612x612&w=0&k=20&c=MaLTbXaYKnDkOWdVZT4MJs9ArMt16NoXK9TnpYK1OAc=",
-        //   preview: true,
-        // },
-        // {
-        //   spotId: 1,
-        //   url: "https://www.gannett-cdn.com/presto/2021/11/22/PNDN/8dd683d3-92e5-4330-ae96-446fac710154-4th_Ave_Great_Room_1128.jpg?width=660&height=440&fit=crop&format=pjpg&auto=webp",
-        //   preview: true,
-        // },
       ],
       {}
     );
